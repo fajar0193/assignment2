4834dfe69deeb46fc1a65bb21f4415eb685bfb1e
fajar0193 <malik.fajar0193@gmail.com>
2019-10-11 13:34:44 -0700

    my first commit

---
+++ 2.py
@@ -0,0 +1,23 @@
+>>> name = input('chuck:')
+chuck
+>>> print('hello chuck!')
+hello chuck!
+>>>
+
+hours = raw_input('35:')
+rate = raw_input('2.75:')
+pay = float(hours) * float(rate)
+print ('pay:')
+
+width=17 
+height=12.0
+width//2
+width/2.0
+height/3
+1+2*5
+
+temp_celcius = raw_input('102C:')
+temp_fahrenheit = (float(temp_celcius) * 1.8) + 32
+print ('this is equivalent to ')
+print temp_fahrenheit,
+print ('gedrees fahrenheit')
